b86b4178bf1ac7a4f0706cb956d997aaec5c9801
庄哈哈orz <1669519973@qq.com>
2025-01-05 16:33:22 +0800

    FactoryBean test

--- spring-part/spring-ioc-xml-01/src/test/java/com/atguigu/test/SpringIocTest.java
+++ spring-part/spring-ioc-xml-01/src/test/java/com/atguigu/test/SpringIocTest.java
@@ -2,6 +2,8 @@ package com.atguigu.test;
 
 import com.atguigu.ioc_03.A;
 import com.atguigu.ioc_03.HappyComponent;
+import com.atguigu.ioc_04.JavaBean2;
+import com.atguigu.ioc_05.JavaBean;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -64,6 +66,38 @@ public class SpringIocTest {
         // 根据接口类型获取bean
         A happyComponent3 = applicationContext.getBean(A.class);
         happyComponent3.doWork();
+
+    }
+
+    @Test
+    public void test_04(){
+        // 1.创建ioc容器 就会进行组件对象的实例化 -> init
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-04.xml");
+
+        // 3.单例 scope=singleton     输出结果是true
+        //   多例 scope=prototype     输出结果是false
+        JavaBean2 bean = applicationContext.getBean(JavaBean2.class);
+        JavaBean2 bean2 = applicationContext.getBean(JavaBean2.class);
+        System.out.println(bean == bean2);
+
+        // 2.正常结束ioc容器
+        applicationContext.close();
+    }
+
+    @Test
+    public void test_05(){
+        // 1.创建ioc实例
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-05.xml");
+
+        // 2.
+        JavaBean javabean = applicationContext.getBean("javaBean", JavaBean.class);
+        System.out.println("javaBean = "+javabean);
+
+        // TODO: FactoryBean工厂也会加入到ioc容器
+        Object bean = applicationContext.getBean("&javaBean");
+        System.out.println("bean" + bean);
+
+
 
     }
 
